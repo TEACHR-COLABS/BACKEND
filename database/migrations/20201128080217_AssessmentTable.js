516420191fb236c97a5513bafc476668cc391f9b
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-11-28 16:23:47 +0000

    seeded data

---
+++ database/migrations/20201128080217_AssessmentTable.js
@@ -0,0 +1,17 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('assessments', table => {
+        table.increments();
+        table.integer('classID', 128).notNullable();
+        table.integer('studentID', 128).notNullable();
+        table.integer('criteriaID', 128).notNullable();
+        table.string('firstName', 128).notNullable();
+        table.string('comments').notNullable();
+        table.date("date").notNullable();
+        table.integer('countChecks', 128).notNullable();
+        table.string('countSelections', 128).notNullable();
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('assessments');
+};
