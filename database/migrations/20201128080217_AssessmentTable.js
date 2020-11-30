4d7d81e889c5521774e42ceaf8a666e1a8402936
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-11-30 01:39:19 +0000

    dATA SEEDING SUCCESSFUL

--- database/migrations/20201128080217_AssessmentTable.js
+++ database/migrations/20201128080217_AssessmentTable.js
@@ -1,6 +1,7 @@
 exports.up = function(knex) {
     return knex.schema.createTable('assessments', table => {
         table.increments();
+        table.integer('userID', 128).notNullable();
         table.integer('classID', 128).notNullable();
         table.integer('studentID', 128).notNullable();
         table.integer('criteriaID', 128).notNullable();
@@ -8,7 +9,6 @@ exports.up = function(knex) {
         table.string('comments').notNullable();
         table.date("date").notNullable();
         table.integer('countChecks', 128).notNullable();
-        table.string('countSelections', 128).notNullable();
     })
 };
 
--- database/seeds/004-AssessmentTable.js
+++ database/seeds/004-AssessmentTable.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('assessments').insert([
-        {classID:1,studentID:1,criteriaID:201,assessmentName:"Composure",comments:"Lonnnnng texttttt thatttt sayyys something",date:"March 15, 2020", countChecks: 1,},
-        {classID:1,studentID:2,criteriaID:205,assessmentName:"Coordination",comments:"Lonnnnng texttttt thatttt sayyys something",date:"September 20, 2020", countChecks: 1,},
-        {classID:2,studentID:3,criteriaID:207,assessmentName:"Organization",comments:"Lonnnnng texttttt thatttt sayyys something",date:"December 15, 2020", countChecks: 1,}
+        {userID:1, classID:1, studentID:1,criteriaID:201,assessmentName:"Composure",comments:"Lonnnnng texttttt thatttt sayyys something",date:"March 15, 2020", countChecks: 1,},
+        {userID:1, classID:1, studentID:2,criteriaID:205,assessmentName:"Coordination",comments:"Lonnnnng texttttt thatttt sayyys something",date:"September 20, 2020", countChecks: 1,},
+        {userID:2, classID:3, studentID:3,criteriaID:207,assessmentName:"Organization",comments:"Lonnnnng texttttt thatttt sayyys something",date:"December 15, 2020", countChecks: 1,}
       ]);
     });
 };
