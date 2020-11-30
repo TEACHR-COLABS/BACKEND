2fffa5884548739b63eb93305877ad68367127a3
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-11-30 02:21:28 +0000

    Get users endpoint

--- api/server.js
+++ api/server.js
@@ -13,7 +13,7 @@ server.use(cors());
 server.use(express.json());
 
 server.get('/', (req, res) => {
-    res.status(200).json({ api: 'up and running!' })
+    res.status(200).json({ api: 'Welcome to teachr app' })
 });
 
 // server.use('/api/auth', authRouter);
--- knexfile.js
+++ knexfile.js
@@ -1,7 +1,7 @@
 // Update with your config settings.
 
 module.exports = {
-
+development: {
   client: 'sqlite3',
     useNullAsDefault: true,
     connection: {
@@ -19,37 +19,5 @@ module.exports = {
         conn.run('PRAGMA foreign_keys = ON', done);
       },
     },
-
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
-
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  }
 };
