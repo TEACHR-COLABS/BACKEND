161a4730e01899c7b813cdee9bf6419312434ae6
Oluwa-Femi <oluwatolablessing@gmail.com>
2020-12-02 23:21:19 +0000

    Files changed for deployment

--- knexfile.js
+++ knexfile.js
@@ -1,23 +1,58 @@
 // Update with your config settings.
 
 module.exports = {
-development: {
-  client: 'sqlite3',
+  development: {
+    client: "sqlite3",
     useNullAsDefault: true,
     connection: {
-      filename: './database/teachr.db3',
+      filename: "./database/teachr.db3",
     },
     migrations: {
-      directory: './database/migrations'
+      directory: "./database/migrations",
     },
-  
+
     seeds: {
-      directory: './database/seeds'
+      directory: "./database/seeds",
     },
     pool: {
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
+        conn.run("PRAGMA foreign_keys = ON", done);
       },
     },
-  }
+  },
+
+  testing: {
+    client: "sqlite3",
+    useNullAsDefault: true,
+    connection: {
+      filename: "./database/test.db3",
+    },
+    migrations: {
+      directory: "./database/migrations",
+    },
+    seeds: {
+      directory: "./database/seeds",
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run("PRAGMA foreign_keys = ON", done);
+      },
+    },
+  },
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    pool: {
+      min: 2,
+      max: 10,
+    },
+    migrations: {
+      directory: "./data/migrations",
+      tableName: "db_migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+    useNullAsDefault: true,
+  },
 };
